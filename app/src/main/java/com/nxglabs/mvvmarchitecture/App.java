b0f5c61bb5ac5e69313e54a04fc7c5bbf0b6185c
Himani <himaniborse1992@gmail.com>
2020-07-02 18:01:14 +0530

    initial commit

---
+++ app/src/main/java/com/nxglabs/mvvmarchitecture/App.java
@@ -0,0 +1,19 @@
+package com.nxglabs.mvvmarchitecture;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private static App sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+    }
+
+    public static App getInstance() {
+        return sInstance;
+    }
+
+}
